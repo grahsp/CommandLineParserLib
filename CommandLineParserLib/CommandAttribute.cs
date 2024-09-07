a4d4df9ade5fc22dff430bc84405081fb21bfb84
grahsp <pontus.grahsp@gmail.com>
2024-09-07 22:49:39 +0200

    Add a CommandAttribute to recognize publicly available methods

---
+++ CommandLineParserLib/CommandAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace CommandLineParserLib
+{
+    [AttributeUsage(System.AttributeTargets.Method, Inherited = false)]
+    public class CommandAttribute : Attribute
+    {
+        public string Name { get; private set; }
+
+        public CommandAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
