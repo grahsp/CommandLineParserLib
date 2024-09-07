f81eacccc31552d7d950c0751c0a40b5241eee0d
grahsp <pontus.grahsp@gmail.com>
2024-09-07 22:35:17 +0200

    Add project files.

---
+++ CommandLineParserLib/CommandLineParser.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace CommandLineParserLib
+{
+    public class CommandLineParser
+    {
+        private string[] _args;
+        public CommandLineParser(string[] args)
+        {
+            _args = args;
+        }
+    }
+}
