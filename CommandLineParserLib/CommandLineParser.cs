eec936dfdcc712d7071c39a458ba3aed0f5d5312
grahsp <pontus.grahsp@gmail.com>
2024-09-08 18:10:54 +0200

    Add basic parsing of arguments

--- CommandLineParserLib/CommandLineParser.cs
+++ CommandLineParserLib/CommandLineParser.cs
@@ -1,31 +1,136 @@
-﻿using System.Diagnostics;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Text.Json;
 
 namespace CommandLineParserLib
 {
     public class CommandLineParser
     {
-        private string[] _args;
-        private Dictionary<string, CommandInfo> _commands;
-
-        private MethodInfo method;
-        private ParameterInfo[] parameters;
-
         public CommandLineParser(string[] args)
         {
-            _args = args;
-            _commands = LoadCommands("C:\\Projects\\C#\\CommandLineParserLib\\CommandLineParserLib\\Commands.json");
+            var commands = LoadCommands("C:\\Projects\\C#\\CommandLineParserLib\\CommandLineParserLib\\Commands.json");
 
-            method = GetMethod(_commands[args[0]]);
-            parameters = method.GetParameters();
+            var method = GetMethod(commands[args[0]]);
+
+            var arguments = ParseArgumentsOfMethod(method, args[1..]);
+            foreach (var arg in arguments)
+            {
+                Console.WriteLine(arg);
+            }
         }
+
+
+        public object[] ParseArgumentsOfMethod(MethodInfo method, string[] args)
+        {
+            var parameters = method.GetParameters();
+
+            var parameterIndicies = MapParameterIndicies(parameters);
+            var (namedArgs, positionalArgs) = ParseArguments(args, parameters, parameterIndicies);
+
+            return ConstructParameterArray(parameters, namedArgs, positionalArgs);
+        }
+
+
+        private object[] ConstructParameterArray(ParameterInfo[] parameters, Dictionary<string, object> namedArgs, List<object> positionalArgs)
+        {
+            var parsedParams = new object[parameters.Length];
+
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                var paramName = parameters[i].Name.ToLower() ?? string.Empty;
+                if (namedArgs.ContainsKey(paramName))
+                {
+                    parsedParams[i] = namedArgs[paramName];
+                }
+                else if (positionalArgs.Count > 0)
+                {
+                    parsedParams[i] = positionalArgs[0];
+                    positionalArgs.RemoveAt(0);
+                }
+                else if (parameters[i].HasDefaultValue)
+                {
+                    parsedParams[i] = parameters[i].DefaultValue!;
+                }
+                else
+                {
+                    throw new Exception($"Parameter '{paramName}' is missing");
+                }
+            }
+
+            return parsedParams;
+        }
+
+
+        private (Dictionary<string, object>, List<object>) ParseArguments(string[] args, ParameterInfo[] parameters, Dictionary<string, int> parameterIndicies)
+        {
+            Dictionary<string, object> namedArgs = [];
+            List<object> positionalArgs = [];
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                var arg = args[i];
+                if (IsNamedArgument(arg))
+                {
+                    var paramName = GetArgumentName(arg);
+                    if (!parameterIndicies.ContainsKey(paramName)) throw new ArgumentException($"Parameter '{paramName}' does not exist");
+
+                    var param = parameters[parameterIndicies[paramName]];
+                    namedArgs[paramName] = ParseArgumentWithValue(args, ref i, param);
+                }
+                else
+                {
+                    positionalArgs.Add(arg);
+                }
+            }
+
+
+            return (namedArgs, positionalArgs);
+        }
+
+        private static object ParseArgumentWithValue(string[] args, ref int index, ParameterInfo parameter)
+        {
+            if (index + 1 < args.Length && !IsNamedArgument(args[index + 1]))
+            {
+                index++;
+                return Convert.ChangeType(args[index], parameter.ParameterType);
+            }
+
+            throw new ArgumentException($"Value for parameter '{parameter.Name}' is missing");
+        }
+
+        private static bool IsNamedArgument(string argument)
+        {
+            return argument.StartsWith('-');
+        }
+
+        private static string GetArgumentName(string argument)
+        {
+            return argument.Trim('-');
+        }
+
+        private static Dictionary<string, int> MapParameterIndicies(ParameterInfo[] parameters)
+        {
+            Dictionary<string, int> parameterIndicies = [];
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                var parameter = parameters[i];
+                var name = parameter.Name ?? string.Empty;
+                var index = i;
+
+                parameterIndicies.Add(name.Trim().ToLower(), index);
+            }
+
+            return parameterIndicies;
+        }
+
+
+
 
         private Dictionary<string, CommandInfo> LoadCommands(string path)
         {
             var jsonString = File.ReadAllText(path);
             return JsonSerializer.Deserialize<Dictionary<string, CommandInfo>>(jsonString, new JsonSerializerOptions { WriteIndented = true }) ?? [];
         }
+
 
         private MethodInfo GetMethod(CommandInfo command)
         {
