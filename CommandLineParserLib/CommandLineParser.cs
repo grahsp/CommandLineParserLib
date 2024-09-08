19354775183aabc1f9e8a5c494dff563cbe7baf0
grahsp <pontus.grahsp@gmail.com>
2024-09-08 21:33:46 +0200

    Add support for bool flags and minor fixes

--- CommandLineParserLib/CommandLineParser.cs
+++ CommandLineParserLib/CommandLineParser.cs
@@ -5,6 +5,8 @@ namespace CommandLineParserLib
 {
     public class CommandLineParser
     {
+        private static readonly JsonSerializerOptions _JsonSerializerOptions = new() { WriteIndented = true };
+
         public CommandLineParser(string[] args)
         {
             var commands = LoadCommands("C:\\Projects\\C#\\CommandLineParserLib\\CommandLineParserLib\\Commands.json");
@@ -19,7 +21,7 @@ namespace CommandLineParserLib
         }
 
 
-        public object[] ParseArgumentsOfMethod(MethodInfo method, string[] args)
+        public static object[] ParseArgumentsOfMethod(MethodInfo method, string[] args)
         {
             var parameters = method.GetParameters();
 
@@ -30,20 +32,20 @@ namespace CommandLineParserLib
         }
 
 
-        private object[] ConstructParameterArray(ParameterInfo[] parameters, Dictionary<string, object> namedArgs, List<object> positionalArgs)
+        private static object[] ConstructParameterArray(ParameterInfo[] parameters, Dictionary<string, string> namedArgs, List<string> positionalArgs)
         {
             var parsedParams = new object[parameters.Length];
 
             for (int i = 0; i < parameters.Length; i++)
             {
-                var paramName = parameters[i].Name.ToLower() ?? string.Empty;
-                if (namedArgs.ContainsKey(paramName))
+                var paramName = parameters[i].Name!.ToLower() ?? string.Empty;
+                if (namedArgs.TryGetValue(paramName, out var _))
                 {
-                    parsedParams[i] = namedArgs[paramName];
+                    parsedParams[i] = Convert.ChangeType(namedArgs[paramName], parameters[i].ParameterType);
                 }
                 else if (positionalArgs.Count > 0)
                 {
-                    parsedParams[i] = positionalArgs[0];
+                    parsedParams[i] = Convert.ChangeType(positionalArgs[0], parameters[i].ParameterType);
                     positionalArgs.RemoveAt(0);
                 }
                 else if (parameters[i].HasDefaultValue)
@@ -60,10 +62,10 @@ namespace CommandLineParserLib
         }
 
 
-        private (Dictionary<string, object>, List<object>) ParseArguments(string[] args, ParameterInfo[] parameters, Dictionary<string, int> parameterIndicies)
+        private static (Dictionary<string, string>, List<string>) ParseArguments(string[] args, ParameterInfo[] parameters, Dictionary<string, int> parameterIndicies)
         {
-            Dictionary<string, object> namedArgs = [];
-            List<object> positionalArgs = [];
+            Dictionary<string, string> namedArgs = [];
+            List<string> positionalArgs = [];
 
             for (int i = 0; i < args.Length; i++)
             {
@@ -71,7 +73,7 @@ namespace CommandLineParserLib
                 if (IsNamedArgument(arg))
                 {
                     var paramName = GetArgumentName(arg);
-                    if (!parameterIndicies.ContainsKey(paramName)) throw new ArgumentException($"Parameter '{paramName}' does not exist");
+                    if (!parameterIndicies.TryGetValue(paramName, out var _)) throw new ArgumentException($"Parameter '{paramName}' does not exist");
 
                     var param = parameters[parameterIndicies[paramName]];
                     namedArgs[paramName] = ParseArgumentWithValue(args, ref i, param);
@@ -86,15 +88,22 @@ namespace CommandLineParserLib
             return (namedArgs, positionalArgs);
         }
 
-        private static object ParseArgumentWithValue(string[] args, ref int index, ParameterInfo parameter)
+        private static string ParseArgumentWithValue(string[] args, ref int index, ParameterInfo parameter)
         {
-            if (index + 1 < args.Length && !IsNamedArgument(args[index + 1]))
+            if (index + 1 < args.Length && !IsNamedArgument(args[index + 1]) && parameter.ParameterType != typeof(bool))
             {
                 index++;
-                return Convert.ChangeType(args[index], parameter.ParameterType);
+                //return Convert.ChangeType(args[index], parameter.ParameterType);
+                return args[index];
+            }
+            // Handle bools without a value
+            else if (parameter.ParameterType == typeof(bool))
+            {
+                return "true";
             }
 
             throw new ArgumentException($"Value for parameter '{parameter.Name}' is missing");
+
         }
 
         private static bool IsNamedArgument(string argument)
@@ -104,7 +113,7 @@ namespace CommandLineParserLib
 
         private static string GetArgumentName(string argument)
         {
-            return argument.Trim('-');
+            return argument.Trim('-').ToLower();
         }
 
         private static Dictionary<string, int> MapParameterIndicies(ParameterInfo[] parameters)
@@ -125,27 +134,20 @@ namespace CommandLineParserLib
 
 
 
-        private Dictionary<string, CommandInfo> LoadCommands(string path)
+        private static Dictionary<string, CommandInfo> LoadCommands(string path)
         {
             var jsonString = File.ReadAllText(path);
-            return JsonSerializer.Deserialize<Dictionary<string, CommandInfo>>(jsonString, new JsonSerializerOptions { WriteIndented = true }) ?? [];
+            return JsonSerializer.Deserialize<Dictionary<string, CommandInfo>>(jsonString, _JsonSerializerOptions) ?? [];
         }
 
 
-        private MethodInfo GetMethod(CommandInfo command)
+        private static MethodInfo GetMethod(CommandInfo command)
         {
             var fullClassName = $"{command.Namespace}.{command.Class}";
 
-            var assembly = Assembly.Load(command.Assembly);
-            if (assembly == null) { throw new ArgumentNullException($"Could not load assembly '{command.Assembly}'"); }
-
-            var type = assembly.GetType(fullClassName);
-            if (type == null) { throw new ArgumentNullException($"Could not load class '{fullClassName}'"); }
-
-            var method = type.GetMethod(command.Method, BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
-            if (method == null) { throw new ArgumentNullException($"Could not load method '{command.Method}' of '{fullClassName}'"); }
-
-            var parameters = method.GetParameters();
+            var assembly = Assembly.Load(command.Assembly) ?? throw new ArgumentNullException($"Could not load assembly '{command.Assembly}'");
+            var type = assembly.GetType(fullClassName) ?? throw new ArgumentNullException($"Could not load class '{fullClassName}'");
+            var method = type.GetMethod(command.Method, BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static) ?? throw new ArgumentNullException($"Could not load method '{command.Method}' of '{fullClassName}'");
 
             return method;
         }
